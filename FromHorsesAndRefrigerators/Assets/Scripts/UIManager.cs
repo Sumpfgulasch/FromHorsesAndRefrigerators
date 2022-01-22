16866e8f5ef3f292ba58fc7cdeb7647232cbd85a
Julian <kontakt@julianhellmann.de>
2022-01-22 21:47:15 +0100

    Further scene setup

--- FromHorsesAndRefrigerators/Assets/Scripts/UIManager.cs
+++ FromHorsesAndRefrigerators/Assets/Scripts/UIManager.cs
@@ -1,18 +1,89 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using Task = System.Threading.Tasks.Task;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CanvasGroup[] screens;
+    public float FadeScreenTime = 0.2f;
+
+    private int activeScreenInt = 0;
+    private CanvasGroup ActiveScreen => screens[activeScreenInt];
+    private CanvasGroup NextScreen => screens[activeScreenInt + 1];
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void FadeToNextScreen()
+    {
+        NextScreen.gameObject.SetActive(true);
+        NextScreen.alpha = 0;
+        FadeToNextCanvasGroup();
+
+    }
+
+    // async Task Test()
+    // {
+    //     await Task.Yield();
+    // }
+
+    private void FadeToNextCanvasGroup()
+    {
+        StartCoroutine(FadeCanvasGroup(screens[activeScreenInt], 1, 0, FadeScreenTime, true, () =>
+            StartCoroutine(FadeCanvasGroup(screens[activeScreenInt], 0, 1, FadeScreenTime))));
+    }
+
+
+    private IEnumerator FadeCanvasGroup(CanvasGroup canvasGroup, float startValue, float targetValue, float time, bool disableWhenFinished = false, Action callback = null)
+    {
+        float timer = 0;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            float value = Mathf.Lerp(startValue, targetValue, timer / time);
+            canvasGroup.alpha = value;
+            yield return null;
+        }
+        canvasGroup.alpha = targetValue;
+
+        if (disableWhenFinished)
+            canvasGroup.gameObject.SetActive(false);
+
+        if (callback != null)
+        {
+            activeScreenInt++;
+            callback.Invoke();
+        }
+
+    }
+
+    // private IEnumerator FadeScreens(CanvasGroup canvasGroup1, CanvasGroup canvasGroup2, float time)
+    // {
+    //     float timer = 0;
+    //     //float startValue = canvasGroup1
+    //     while (timer < time)
+    //     {
+    //         timer += Time.deltaTime;
+    //         float value = Mathf.Lerp(startValue, targetValue, timer / time);
+    //         canvasGroup1.alpha = value;
+    //         yield return null;
+    //     }
+    //     canvasGroup.alpha = targetValue;
+    //
+    //
+    // }
+
 }
